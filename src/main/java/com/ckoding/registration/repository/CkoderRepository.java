4e8fb50be32e6007d6a4403625c6c140ba924cc0
derrick070 <derrickdsouza070@gmail.com>
2020-05-04 12:16:11 +0530

    repository filed name type change

--- src/main/java/com/ckoding/registration/repository/CkoderRepository.java
+++ src/main/java/com/ckoding/registration/repository/CkoderRepository.java
@@ -1,9 +1,10 @@
 package com.ckoding.registration.repository;
 
 import com.ckoding.registration.entity.Ckoder;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CkoderRepository extends JpaRepository<Ckoder, Integer> {
+public interface CkoderRepository extends JpaRepository<Ckoder, UUID> {
 
   Ckoder findByEmailId(String emailId);
 }
